0242e3fdafd95d9c1faa95e69412ace619f6f945
Thomas <53497553+thomasandrew@users.noreply.github.com>
2024-09-18 12:50:52 -0300

    Add files via upload

---
+++ src/theme/index.js
@@ -0,0 +1,24 @@
+export const lightTheme = {
+    body: '#F2F2F2',
+    nav: '#fff',
+    text: '#363537',
+    icon: '#555',
+    icone: "#363537",
+    toggleBorder: '#97BEE6',
+    background: '#363537',
+    borderColor: '#363537',
+    sobrejd: '#F2F2F2',
+    imgAtualizacao: './img_sicronismo/sincronismo_two.png',
+}
+export const darkTheme = {
+    body: '#363537',
+    nav: '#444',
+    text: '#FAFAFA',
+    icon: '#999',
+    icone: "#fff",
+    toggleBorder: '#6B8096',
+    background: '#999',
+    borderColor: '#F2F2F2',  
+    sobrejd: '#fff',
+    imgAtualizacao: './img_sicronismo/sincronismo.png',
+}
