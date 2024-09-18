44f9803eb635c83e208ead2921113b96aee2551d
Thomas <53497553+thomasandrew@users.noreply.github.com>
2024-09-18 12:43:06 -0300

    Add files via upload

---
+++ src/components/assets/index.js
@@ -0,0 +1,8 @@
+export const icons = {
+    app: require('../../../assets/icon.png'),
+}
+
+export const imgs = {
+    logoWSGE: require('../../../assets/img/logoWSGE.png'),
+    logoWSGE_cinza: require('../../../assets/img/logoWSGE_cinza.png'),
+}
