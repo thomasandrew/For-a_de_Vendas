fed02ed687cb6e2aef29cb573e871f206116464b
Thomas <53497553+thomasandrew@users.noreply.github.com>
2024-09-18 14:07:29 -0300

    Add files via upload

---
+++ .history/src/screens/Menu/Item_menu/MenuItem_20201007170209.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { View, ScrollView, StyleSheet, Image } from "react-native";
+
+import { useDarkMode } from "../../providers/DarkModeProvider";
+
+export default function ({ navigation }) {
+  const { darkMode, setDarkMode } = useDarkMode();
+  return (
+    <View style={styles.menuItem}>
+      <Image source={this.props.itemImage} style={styles.image} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  menuItem: {
+    width: "33.333333%",
+    height: "50%",
+    padding: 20,
+    backgroundColor: '#ccc',
+    borderColor: '#000',
+    borderWidth: 2, // remember
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+    opacity: 0.8,
+    borderColor: "#fff",
+    borderWidth: 3,
+  },
+});
---
+++ .history/src/screens/Menu/Item_menu/MenuItem_20201007173112.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { View, ScrollView, StyleSheet, Image } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { Screen, Container, Text } from "../../../components/ui";
+
+import { useDarkMode } from "../../../providers/DarkModeProvider";
+
+export default function ({ navigation }, props) {
+  const { darkMode, setDarkMode } = useDarkMode();
+  return (
+    <View style={styles.menuItem}>
+      <TouchableOpacity> 
+          <Image source={props.itemImage} style={styles.image} />
+      </TouchableOpacity> 
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  menuItem: {
+    width: "33.333333%",
+    height: "50%",
+    padding: 20,
+    //backgroundColor: '#ccc',
+    //borderColor: '#000',
+    //borderWidth: 2, 
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+    opacity: 0.8,
+    borderColor: "#fff",
+    borderWidth: 3,
+  },
+});
